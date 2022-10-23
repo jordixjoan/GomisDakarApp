d641b5465d56623da8fcb39a5dc4b2e5b13baaf0
jordiventura <jordixjoan@gmail.com>
2022-10-23 19:15:49 +0200

    Commit inicial con Spring Boot

---
+++ GomisDakarTabletApp/src/test/java/com/GomisDakarTabletApp/GomisDakarTabletApp/GomisDakarTabletAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GomisDakarTabletApp.GomisDakarTabletApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GomisDakarTabletAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
